7567b966bc0accd59e3aa107267ce615e21e1dce
Luis Gonzalez <lgonzalez23@verizon.net>
2024-02-12 12:06:53 -0800

    sidebars heading

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const RightSidebar = () => {
-  return <div>RightSidebar</div>;
+  return (
+    <section className='flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-w-[227px] sticky right-0 h-full max-sm:hidden select-none overflow-y-auto pb-20'>
+      <h3 className='text-center pt-4 text-xs uppercase'>
+        Design
+      </h3>
+    </section>
+  );
 };
 
 export default RightSidebar;
