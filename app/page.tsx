2df53b4107c084795960f2494c16148d02dbc9de
Luis Gonzalez <lgonzalez23@verizon.net>
2024-02-13 12:29:48 -0800

    canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,47 @@
 "use client";
 
+import { fabric } from "fabric";
+
+import { useEffect, useRef } from "react";
+
 import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
+import {
+  handleCanvasMouseDown,
+  handleResize,
+  initializeFabric,
+} from "@/lib/canvas";
 
 export default function Home() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricRef = useRef<fabric.Canvas | null>(null);
+  const isDrawing = useRef(false);
+  const shapeRef = useRef<fabric.Object | null>(null);
+  const selectedShapeRef = useRef<string | null>(null);
+
+  useEffect(() => {
+    const canvas = initializeFabric({
+      canvasRef,
+      fabricRef,
+    });
+
+    canvas.on("mouse:down", (options) => {
+      handleCanvasMouseDown({
+        canvas,
+        isDrawing,
+        options,
+        selectedShapeRef,
+        shapeRef,
+      });
+    });
+
+    window.addEventListener("resize", () => {
+      handleResize({ canvas });
+    });
+  }, []);
+
   return (
     <main className='h-screen overflow-hidden'>
       <Navbar />
